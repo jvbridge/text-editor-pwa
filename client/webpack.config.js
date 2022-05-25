feb36852522cb5d48bc32ceb43941ca093f1918b
Jonathan Bridge <jvbridge@gmail.com>
2022-05-24 21:41:52 -0700

    actually made the inject manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,10 +34,10 @@ module.exports = () => {
           // },
         ],
       }),
-      // new InjectManifest({
-      //   swSrc: "./src-sw.js",
-      //   swDest: "service-worker.js",
-      // }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "dist/service-worker.js",
+      }),
     ],
 
     module: {
