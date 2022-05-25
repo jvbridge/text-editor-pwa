d62cbf7b25e9dfa2ad115a469d7766a453d55ecb
Jonathan Bridge <jvbridge@gmail.com>
2022-05-24 22:18:17 -0700

    fixed delete and put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("putDb not implemented");
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
   const jateDb = await openDB("jate", 1);
@@ -31,9 +35,9 @@ export const getDb = async () => {
 
 export const deleteDb = async (id) => {
   console.log("DELETE from the database", id);
-  const todosDb = await openDB("todos", 1);
-  const tx = todosDb.transaction("todos", "readwrite");
-  const store = tx.objectStore("todos");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.delete(id);
   const result = await request;
   console.log("result.value", result);
